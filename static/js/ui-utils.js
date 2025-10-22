0c22a58302ce0fa5915e32863d7b29ec6e12b7b5
Willis Chan <willis.chan@hotmail.com.hk>
2025-10-22 12:40:27 +0800

    Phase 2 JS Migration: Move refreshDisplay() to ui-utils.js
    - Created new static/js/ui-utils.js file
    - Moved refreshDisplay() function to external file
    - Added script tag to load ui-utils.js
    - Function made globally available via window object
    - Tested and working correctly
    - Version: 3.9.2

---
+++ static/js/ui-utils.js
@@ -0,0 +1,10 @@
+// TechCafeBooking - UI Utilities
+// UI helper functions and display management
+
+// Display refresh utilities
+function refreshDisplay() {
+    updateTimeSlots();
+}
+
+// Make function globally available
+window.refreshDisplay = refreshDisplay;
